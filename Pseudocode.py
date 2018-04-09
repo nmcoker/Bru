837925eeda93e0b102b12cbf26d7f152b4dd9376
nmcoker <38221406+nmcoker@users.noreply.github.com>
2018-04-09 16:10:22 -0500

    Create Pseudocode.py

---
+++ Pseudocode.py
@@ -0,0 +1,14 @@
+'''import database'''
+
+def brew()
+  ''' reset door
+  push button 1
+  push button 2
+  global var = 0'''
+  
+while(true)
+  if(global var == 1)
+	  brew()
+  else
+    '''wait'''
+
